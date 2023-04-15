c1ebfa9bcc3af9b4c419cc4be9a7891bc2b6d4c6
Your Name <Nurul Safitri>
2023-04-15 13:30:53 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Biodata Diri</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Biodata Diri</h1>
+		<img src="" alt="Foto Diri" width="150">
+		<table>
+			<tr>
+				<th>Nama:</th>
+				<td>Nurul Safitri Ariani</td>
+			</tr>
+			<tr>
+				<th>Tempat, Tanggal lahir@</th>
+				<td>Duri, 18 Desember 2001</td>
+			</tr>
+			<tr>
+				<th>Jenis Kelamin:</th>
+				<td>Perempuan</td>
+			</tr>
+			<tr>
+				<th>Alamat:</th>
+				<td>Jl. Harapan Baru, Gg. jambu, Pekanbaru, Riau.</td>
+			</tr>
+			<tr>
+            <th>Riwayat Pendidikan:</th>
+				<td>
+					<ul>
+						<li>SDN 14 Talang Mandi</li>
+						<li>SMPN 2 Mandau</li>
+						<li>SMAN 1 MANDAU</li>
+                        <li>Universitas Alma Ata Yogyakarta</li>
+					</ul>
+				</td>
+			</tr>
+			<tr>
+				<th>Hobby</th>
+				<td>Berenang</td>
+                <td>Travelling</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
